f03fd0c157c1e01002412350957faa92d38d335f
Flailton Batista <flailton@gmail.com>
2021-04-06 05:10:49 -0300

    SQL adjustments
    SQL adjustments

--- classes/output/goalprogress.php
+++ classes/output/goalprogress.php
@@ -82,7 +82,7 @@ class goalprogress extends controller
         $goal = $DB->get_record('bga_goals', ['id' => $data->goalid]);
         $accruedprogress = $DB->get_record_sql('
             SELECT IFNULL(SUM(gp.progress), 0) as total
-            FROM moodle.mdl_bga_goal_progress gp
+            FROM mdl_bga_goal_progress gp
             WHERE gp.goalid = :goalid ',  
             ['goalid' => $data->goalid ]
         );
